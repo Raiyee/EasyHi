761a9414a6128808121b0f101f3900169d6e7638
JounQin <admin@1stg.me>
2016-08-16 17:51:17 +0800

    删除 docs 脚本, 调整 push 脚本

--- build/bash/push.sh
+++ build/bash/push.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
-npm run lint
-
 set -e
 echo "Enter message: "
 read MESSAGE
 
 echo "Pushing $MESSAGE ..."
 
-npm run clean
+#build
 npm run build
 
-cd dist
-
-git init
-
 # commit
+cd dist
+git init
 git add -A
 git commit -m "$MESSAGE"
 
 # push
 git push --force --quiet git@github.com:raiyee/EasyHi.git master:gh-pages
 git push --force --quiet git@git.coding.net:Raiyee/EasyHi.git master:coding-pages
+
+# back to root
+cd ..
