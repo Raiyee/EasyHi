f346f3e99c351592c487f89763d38adcf087c7e1
JounQin <admin@1stg.me>
2016-08-18 13:12:46 +0800

    简单优化

--- build/bin/stylelint.js
+++ build/bin/stylelint.js
@@ -3,4 +3,4 @@ const debug = require('debug')('koa:stylelint');
 require('stylelint').lint({
   files: ['css', 'less', 'sass', 'scss', 'styl', 'stylus'].map(value => 'src/**/*\\.' + value),
   formatter: 'verbose'
-}).then(result => result.errored && debug('there are some errors occurred!%s', result.output));
+}).then(result => result.errored && (debug('there are some errors occurred!%s', result.output) || process.exit(1)));
--- build/config/_test.js
+++ build/config/_test.js
@@ -1,4 +1,7 @@
 export default config => ({
   compiler_devtool: 'source-map',
-  compiler_public_path: `http://${config.server_host || 'localhost'}:${config.server_port}/`
+  compiler_hash_type: 'chunkhash',
+  compiler_html_minify: true,
+  compiler_public_path: ''
+  // compiler_public_path: `http://${config.server_host || 'localhost'}:${config.server_port}/`
 });
