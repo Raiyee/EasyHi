85429fde9eae2a1745e6746e9f74dadcbc9e73fd
JounQin <admin@1stg.me>
2016-08-20 20:18:20 +0800

    优化公共样式

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -13,26 +13,30 @@ export default {
         <div class={['center-block img-circle', classes.yoga]}>
           <img class="img-circle" src={yoga} alt="瑜伽"/>
         </div>
-        <form on-submit={this.submit}>
-          <div class="form-group">
-            <div class="input-group">
-            <span class="input-group-addon">
-              <span class="glyphicon glyphicon-phone"/>
-            </span>
-              <input type="number" class="form-control" placeholder="请输入手机号"/>
-            </div>
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-sm-push-3 col-lg-4 col-lg-push-4">
+            <form on-submit={this.submit}>
+              <div class="form-group">
+                <div class="input-group">
+                  <span class="input-group-addon">
+                    <span class="glyphicon glyphicon-phone"/>
+                  </span>
+                  <input type="number" class="form-control" placeholder="请输入手机号"/>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="input-group">
+                  <span class="input-group-addon">
+                    <span class="glyphicon glyphicon-lock"/>
+                  </span>
+                  <input type="password" class="form-control" placeholder="请输入验证码"/>
+                  <span class="input-group-addon theme-color">获取验证码</span>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-default center-block">Submit</button>
+            </form>
           </div>
-          <div class="form-group">
-            <div class="input-group">
-            <span class="input-group-addon">
-              <span class="glyphicon glyphicon-lock"/>
-            </span>
-              <input type="password" class="form-control" placeholder="请输入验证码"/>
-              <span class="input-group-addon theme-color">获取验证码</span>
-            </div>
-          </div>
-          <button type="submit" class="btn btn-default center-block">Submit</button>
-        </form>
+        </div>
       </div>
     );
   }
