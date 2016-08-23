00c6926b3bc6fb2faaea7b79fdae070af44eec0a
shuxun <yinsx@raiyee.com>
2016-08-23 08:46:31 +0800

    提供微官网入口

---
+++ src/views/MerchantWebsite/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class=container>
+      这是微官网首页
+    </div>
+</template>
+<style>
+    body{
+        background-color:#ff0000;
+    }
+</style>
+<script>
+
+</script>
+<style lang="styl" src="./index.styl" scoped/>
+
