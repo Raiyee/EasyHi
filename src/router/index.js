66db6daded39a4c943c66d7a331fbbbeddf5b04d
JounQin <admin@1stg.me>
2016-08-30 21:42:44 +0800

    优化 Loading 组件样式, 将 routes 配置提出来

--- src/router/index.js
+++ src/router/index.js
@@ -1,61 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import store from 'store';
-
 Vue.use(VueRouter);
 
-const router = new VueRouter({
-  mode: 'hash',
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: resolve => require(['views/MemberIndex'], resolve)
-    }, {
-      path: '/login',
-      name: 'login',
-      component: resolve => require(['views/Login'], resolve)
-    }, {
-      path: '/member-center',
-      name: 'memberCenter',
-      component: resolve => require(['views/MemberCenter'], resolve),
-      meta: {
-        auth: true,
-        init: {
-          url: '/membercenter',
-          restore: false
-        }
-      }
-    }, {
-      path: '/member-subscribe',
-      name: 'memberSubscribe',
-      component: resolve => require(['views/MemberSubscribe'], resolve),
-      meta: {
-        bg: false,
-        init: {
-          url: '/get-schedules',
-          actions: {
-            addStatuses: 'calendarStatus'
-          }
-        }
-      }
-    }, {
-      path: '/website',
-      name: 'website',
-      component: resolve => require(['views/MerchantWebsite'], resolve),
-      meta: {
-        init: {
-          url: '/get-website-edit',
-          restore: false
-        }
-      }
-    }, {
-      path: '*',
-      redirect: '/'
-    }
-  ]
-});
+import store from 'store';
+import routes from './routes';
+
+const router = new VueRouter(routes);
 
 const body = document.getElementsByTagName('body')[0];
 
