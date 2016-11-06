e7f4e26afebbf6aa0a5af121347f377f2b5f638d
JounQin <admin@1stg.me>
2016-11-06 20:28:03 +0800

    log

--- build/bash/sync.sh
+++ build/bash/sync.sh
@@ -5,11 +5,17 @@ set -e
 (git log -1 --pretty=%B | cat) |
 while read -r MESSAGE
 do
+  echo "last commit message:"
+  echo "$MESSAGE"
+
   cd dist
   cp ../*.md .
+
   git init
   git add -A
   git commit -m "$MESSAGE"
   git push --force --quiet git@github.com:Raiyee/EasyHi.git master:gh-pages
   git push --force --quiet git@git.coding.net:Raiyee/EasyHi.git master:coding-pages
+
+  exit 0
 done
