baf8774fe6231b0b4a4a62fe333a05856b9225f2
JounQin <admin@1stg.me>
2016-11-06 19:24:36 +0800

    vue-loader now supports cssModules officially

--- build/bash/push.sh
+++ build/bash/push.sh
@@ -8,29 +8,30 @@ echo "Pulling from origin/master"
 git pull
 
 #build
-yarn outdated
-npm run build
+#yarn outdated
+#npm run build
 
-echo "Enter message: "
-read MESSAGE
+(git log -1 --pretty=%B | cat) |
+while read -r MESSAGE
+do
+  echo "Pushing $MESSAGE ..."
 
-echo "Pushing $MESSAGE ..."
+  git commit -a -m "$MESSAGE"
+  git push --force --quiet git@github.com:Raiyee/EasyHi.git master:master
+  git push --force --quiet git@git.coding.net:Raiyee/EasyHi.git master:master
 
-git commit -a -m "$MESSAGE"
-git push --force --quiet git@github.com:Raiyee/EasyHi.git master:master
-git push --force --quiet git@git.coding.net:Raiyee/EasyHi.git master:master
+  # commit
+  cd dist
+  #cp ../README.md .
+  cp ../*.md .
+  git init
+  git add -A
+  git commit -m "$MESSAGE"
 
-# commit
-cd dist
-#cp ../README.md .
-cp ../*.md .
-git init
-git add -A
-git commit -m "$MESSAGE"
+  # push
+  git push --force --quiet git@github.com:Raiyee/EasyHi.git master:gh-pages
+  git push --force --quiet git@git.coding.net:Raiyee/EasyHi.git master:coding-pages
 
-# push
-git push --force --quiet git@github.com:Raiyee/EasyHi.git master:gh-pages
-git push --force --quiet git@git.coding.net:Raiyee/EasyHi.git master:coding-pages
-
-# back to root
-cd ..
+  # back to root
+  cd ..
+done
