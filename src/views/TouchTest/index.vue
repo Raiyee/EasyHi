439c5efdbbe465bdb8f1c0d987e699dcd9d956c3
JounQin <admin@1stg.me>
2016-11-09 21:05:00 +0800

    继续完善 v-touch 插件，丰富演示页面内容

--- src/views/TouchTest/index.vue
+++ src/views/TouchTest/index.vue
@@ -1,59 +1,70 @@
 <template>
-  <div>
+  <div class="container">
+    tap: {{ tap }},<br>
+    dbTap: {{ dbTap }},<br>
+    press: {{ press }},<br>
+    swipeLeft: {{ swipeLeft }},<br>
+    swipeRight: {{ swipeRight }},<br>
+    swipeUp: {{ swipeUp }},<br>
+    swipeDown: {{ swipeDown }}
     <br>
-    <button class="btn btn-primary" :class="$style.btn" v-touch="touchOptions">
-      {{ msg }}, {{ x + y }}
+    <button class="btn btn-primary" :class="$style.btn" v-touch="touch">
+      {{ msg }}
     </button>
-    <span>{{ x }}, {{ y }}</span>
     <br>
     <br>
-    <button class="btn btn-default" @click="add('x')">add x</button>
-    <button class="btn btn-default" @click="add('y')">add y</button>
+    <button class="btn btn-default" @click="toggle('x')">toggle x ({{ x }})</button>
+    <button class="btn btn-default" @click="toggle('y')">toggle y ({{ y }})</button>
   </div>
 </template>
 <script>
   export default {
     data() {
       return {
-        msg: 'try to move me!',
-        x: 0,
-        y: 0
+        msg: 'try to touch, move, swipe, press me!',
+        x: false,
+        y: false,
+        tap: 0,
+        dbTap: 0,
+        press: 0,
+        swipeLeft: 0,
+        swipeRight: 0,
+        swipeUp: 0,
+        swipeDown: 0
       }
     },
     computed: {
-      touchOptions() {
+      touch() {
         return {
-          x: false,
-          y: false,
-          handler: {
-            tap() {
-              console.log('tap')
-            },
-            dbTap() {
-              console.log('dbTap')
-            },
-            press() {
-              console.log('press')
-            },
-            swipeLeft() {
-              console.log('swipeLeft')
-            },
-            swipeRight() {
-              console.log('swipeRight')
-            },
-            swipeUp() {
-              console.log('swipeUp')
-            },
-            swipeDown() {
-              console.log('swipeDown')
-            }
+          x: this.x,
+          y: this.y,
+          tap: () => {
+            this.tap++
+          },
+          dbTap: () => {
+            this.dbTap++
+          },
+          press: () => {
+            this.press++
+          },
+          swipeLeft: () => {
+            this.swipeLeft++
+          },
+          swipeRight: () => {
+            this.swipeRight++
+          },
+          swipeUp: () => {
+            this.swipeUp++
+          },
+          swipeDown: () => {
+            this.swipeDown++
           }
         }
       }
     },
     methods: {
-      add(prop) {
-        this[prop]++
+      toggle(prop) {
+        this[prop] = !this[prop]
       }
     }
   }
