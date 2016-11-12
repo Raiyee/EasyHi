9096c38046489858bcff6f1da86866e60131a790
JounQin <admin@1stg.me>
2016-11-12 22:45:09 +0800

    test in hi.1stg.me

--- build/config/_test.js
+++ build/config/_test.js
@@ -2,6 +2,6 @@ export default config => ({
   compiler_devtool: 'source-map',
   compiler_hash_type: 'chunkhash',
   compiler_html_minify: true,
-  // compiler_public_path: ''
-  compiler_public_path: `http://${config.server_host || 'localhost'}:${config.server_port}/`
+  compiler_public_path: ''
+  // compiler_public_path: `http://${config.server_host || 'localhost'}:${config.server_port}/`
 })
