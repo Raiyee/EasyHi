25c21cc49b99a5e25ecc4ac92d601d0daaea8948
JounQin <admin@1stg.me>
2016-11-17 18:05:01 +0800

    引入 iconfont (theme 样式无法单独引入，需要思考如何进行)

---
+++ src/components/HiLoading/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="[$style.hiLoading, className]">
+    <div :class="$style['hi-loading-' + theme]"></div>
+    <slot/>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'hi-loading',
+    props: {
+      className: {
+        type: String,
+        default: ''
+      },
+      theme: {
+        type: String,
+        default: 'snake'
+      }
+    }
+  }
+</script>
+<style src="./index.css" module/>
