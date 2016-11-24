61f82846b40b0c91b0beb05e37fe61f117f77624
xieww <xieww@raiyee.com>
2016-11-24 16:24:48 +0800

    ocnfirm, tip, toast

---
+++ src/utils/modal.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+
+let modalOptions = {
+  backdrop: true,
+  transition: 'bounce',
+  show: true
+}
+
+let mergeProps = (propOptions) => {
+  return {
+    modalHeader: propOptions.modalHeader || '头部信息',
+    tipText: propOptions.tipText || '系统消息',
+    confirmBtn: propOptions.confirmBtn || '确定',
+    cancelBtn: propOptions.cancelBtn || '取消',
+    afterConfirm: propOptions.afterConfirm,
+    afterCancel: propOptions.afterCancel
+  }
+}
+export const confirmOn = (options) => {
+  return Vue.prototype.$modal.open({
+    component: System.import('../views/_Modal/ConfirmModal'),
+    options: Object.assign(modalOptions, options.options),
+    props: mergeProps(options)
+  })
+}
+
+export const tipOn = (options) => {
+  return Vue.prototype.$modal.open({
+    component: System.import('../views/_Modal/ConfirmModal'),
+    options: Object.assign(modalOptions, options.options),
+    props: Object.assign(mergeProps(options), {isTipModal: true})
+  })
+}
+
+export const toastOn = (tipText, remove, timeoutMill) => {
+  return Vue.prototype.$modal.open({
+    component: System.import('../views/_Modal/ConfirmModal'),
+    options: modalOptions,
+    props: {
+      tipText: tipText,
+      isToastModal: true,
+      timeoutMill: timeoutMill,
+      remove: remove
+    }
+  })
+}
--- src/views/_Modal/index.vue
+++ src/views/_Modal/index.vue
@@ -2,9 +2,13 @@
   <div>
     <button class="btn btn-theme-primary" @click="addModal">Add modal</button>
     <button class="btn btn-theme-default" @click="clearModal">Clear modal</button>
+    <button class="btn btn-theme-default" @click="confirmModal">Confirm modal</button>
+    <button class="btn btn-theme-default" @click="tipModal">Tip modal</button>
+    <button class="btn btn-theme-default" @click="toastModal">Toast modal</button>
   </div>
 </template>
 <script>
+  import {confirmOn, tipOn, toastOn} from 'utils'
   let modalId
 
   export default {
@@ -30,6 +34,33 @@
       },
       clearModal() {
         this.$modal.clear()
+      },
+      confirmModal() {
+        modalId = confirmOn({
+          tipText: 'I am  a confirm modal tip Text',
+          confirmBtn: '蓝瘦',
+          afterConfirm() {
+            console.log('It is after confirm modal')
+            this.$modal.close(modalId)
+          },
+          afterCancel() {
+            console.log('It is after cancel modal')
+          }
+        })
+      },
+      tipModal() {
+        modalId = tipOn({
+          tipText: 'I am  a confirm modal tip Text',
+          afterConfirm() {
+            console.log('It is after tip modal')
+            this.$modal.close(modalId)
+          }
+        })
+      },
+      toastModal() {
+        modalId = toastOn('I am  a confirm modal tip Text', function () {
+          console.log('It is a toast')
+        }, 2000)
       }
     }
   }
