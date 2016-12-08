3b5c4e22f0f9ad3a843ccdf4f28957c39dd9c833
JounQin <admin@1stg.me>
2016-12-08 17:14:17 +0800

    shorten

--- packages/vue-template-es2015-loader/index.js
+++ packages/vue-template-es2015-loader/index.js
@@ -1,7 +1,5 @@
-const compiler = require('vue-loader/lib/template-compiler')
-
 module.exports = function (content) {
   if (!this.options) this.options = {}
   if (!this.options.__vueOptions__) this.options.__vueOptions__ = {}
-  return compiler.call(this, content)
+  return require('vue-loader/lib/template-compiler').call(this, content)
 }
