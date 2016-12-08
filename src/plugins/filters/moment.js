470090af46bd72454c9535b72ecf24a0d4be1ed6
JounQin <admin@1stg.me>
2016-12-08 23:58:02 +0800

    rewrite component of MemberMessage

--- src/plugins/filters/moment.js
+++ src/plugins/filters/moment.js
@@ -2,4 +2,5 @@ import {getWeekday, formatDate, isYesterday, isToday, isTomorrow} from 'utils'
 
 export {getWeekday, formatDate}
 
-export const dateText = (date, format) => isYesterday(date) ? '昨天' : isToday(date) ? '今天' : isTomorrow(date) ? '明天' : formatDate(date, format)
+export const dateText = (date, format) =>
+  isYesterday(date) ? '昨天' : isToday(date) ? '今天' : isTomorrow(date) ? '明天' : formatDate(date, format)
