3cf2e5cdcf8484a8676d29d06c0dbd8515dabcf9
xieww <xieww@raiyee.com>
2016-12-09 09:48:21 +0800

    member-message tiny adjustment

--- src/views/MemberCenter/MemberMessage/index.js
+++ src/views/MemberCenter/MemberMessage/index.js
@@ -1,16 +1,3 @@
-const MSG_TYPES = {
-  S: '上课提醒',
-  Q: '取消预订消息',
-  Y: '预订消息',
-  W: '温馨提示',
-  B: '卡变更提醒',
-  D: '到期提醒',
-  X: '私教课时间修改',
-  Z: '中奖提醒',
-  G: '购买成功提醒',
-  R: '退款提醒'
-}
-
 export default {
   ...require('./index.pug'),
   name: 'memberMessage',
@@ -19,8 +6,5 @@ export default {
       classes: require('./index.styl'),
       ...this.$route.meta.data
     }
-  },
-  filters: {
-    msgType: type => MSG_TYPES[type] || '消息提醒'
   }
 }
--- src/views/MemberCenter/MemberMessage/mock.js
+++ src/views/MemberCenter/MemberMessage/mock.js
@@ -3,18 +3,33 @@ import moment from 'moment'
 
 const Random = Mock.Random
 
-Mock.mock(/\/membermessage$/, () => ({
-  'messages': new Array(Random.integer(0, 5)).fill(0).map(() => {
-    let date = moment().add(-Random.integer(0, 3), 'd').format('YYYY-MM-DD')
-    return {
-      date,
-      msgs: new Array(Random.integer(1, 3)).fill(0).map(() => ({
-        messageId: Random.character('number', 11),
-        msgContent: Random.cword(5, 12),
-        read: Random.boolean(),
-        type: Random.character('SQYWBDXZGRVX'),
-        createTime: +Random.date('T')
-      }))
-    }
-  })
-}))
+const MESSAGE_TYPE = [
+  '上课提醒',
+  '取消预订消息',
+  '预订消息',
+  '温馨提示',
+  '卡变更提醒',
+  '到期提醒',
+  '私教课时间修改',
+  '中奖提醒',
+  '购买成功提醒',
+  '退款提醒']
+
+Mock.mock(/\/membermessage$/, () => {
+  let now = moment()
+  return {
+    messages: new Array(Random.integer(0, 5)).fill(0).map(() => {
+      let date = now.add(-Random.integer(1, 3), 'd').format('YYYY-MM-DD')
+      return {
+        date,
+        msgs: new Array(Random.integer(1, 3)).fill(0).map(() => ({
+          messageId: Random.character('number', 11),
+          messageContent: Random.cword(5, 12),
+          read: Random.boolean(),
+          type: MESSAGE_TYPE[Random.integer(0, MESSAGE_TYPE.length)],
+          createTime: +Random.date('T')
+        }))
+      }
+    })
+  }
+})
