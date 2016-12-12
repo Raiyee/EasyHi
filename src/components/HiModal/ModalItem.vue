daf1a9b997c4427c3fc50dbf085cc0af0faef6ea
JounQin <admin@1stg.me>
2016-12-12 16:22:46 +0800

    CSS modules in .vue(lint fixed)

--- src/components/HiModal/ModalItem.vue
+++ src/components/HiModal/ModalItem.vue
@@ -1,5 +1,7 @@
 <template src="./modal-item.pug"/>
 <script>
+  import {isEmptyStr, warn, error} from 'utils'
+
   export default {
     props: {
       id: [Number, String],
@@ -14,17 +16,17 @@
     computed: {
       label() {
         const header = this.header
-        return this.$util.isEmptyStr(header) ? '&nbsp;' : header
+        return isEmptyStr(header) ? '&nbsp;' : header
       }
     },
     methods: {
       closeModal() {
         this.close ? this.close.apply(this, arguments)
-          : this.$modal.close(this.id || this.$util.warn('there is no modal id found, then the current modal will be close!'))
+          : this.$modal.close(this.id || warn('there is no modal id found, then the current modal will be close!'))
       },
       confirmModal() {
         this.confirm ? this.confirm.apply(this, arguments)
-          : this.$util.error('you should handle the click event on the confirm btn by yourself!')
+          : error('you should handle the click event on the confirm btn by yourself!')
       }
     }
   }
