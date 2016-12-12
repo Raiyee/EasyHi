1edb3dddb27bc81556b3d0eeba147d45e6f8d8f7
JounQin <admin@1stg.me>
2016-12-12 16:36:39 +0800

    fix issue of prompt component

--- src/utils/promot.js
+++ src/utils/promot.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 
 ['toast', 'tip', 'confirm', 'prompt'].forEach((value, type) => {
   module.exports[value] = props => Vue.prototype.$modal.open({
-    component: System.import('../components/HiModal/PromptModal'),
+    component: System.import('components/HiModal/PromptModal'),
     options: {
       backdrop: true,
       show: true,
       destroy: true
     },
-    props: Object.assign({transition: true}, props, {type})
+    props: {transition: true, ...props, type}
   })
 })
