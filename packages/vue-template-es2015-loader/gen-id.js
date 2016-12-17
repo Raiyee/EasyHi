e9398dca9af83be815760007b8811ededf3bec2b
JounQin <admin@1stg.me>
2016-12-17 10:00:53 +0800

    fix issue of chunkhash changes everytime

--- packages/vue-template-es2015-loader/gen-id.js
+++ packages/vue-template-es2015-loader/gen-id.js
@@ -1,6 +1,8 @@
-let uid = 0
-const uidRegistry = Object.create(null)
+// utility for generating a uid for each component file
+// used in scoped CSS rewriting
+const hash = require('hash-sum')
+const cache = Object.create(null)
 
-module.exports = function genId(filePath) {
-  return uidRegistry[filePath] || (uidRegistry[filePath] = ++uid)
+module.exports = function genId(file) {
+  return cache[file] || (cache[file] = hash(file))
 }
