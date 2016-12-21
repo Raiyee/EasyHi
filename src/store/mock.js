8e573c3f5ae5d56b407bdb82f35b3d71384f2784
JounQin <admin@1stg.me>
2016-12-21 15:35:18 +0800

    mock parse

--- src/store/mock.js
+++ src/store/mock.js
@@ -3,8 +3,9 @@ import Mock from 'mockjs'
 const TCODES = [12345678910]
 const VALID_BASE = TCODES.map(tcode => `/yoga-vision/${tcode}`)
 
-Mock.mock(/\/initialize$/, ({data}) => {
+Mock.mock(/\/initialize$/, ({body}) => {
+  body = JSON.parse(body)
   return {
-    error: VALID_BASE.includes(data.base) ? '' : '未找到符合的商户，请确认 url 是否正确！'
+    error: VALID_BASE.includes(body.base) ? '' : '未找到符合的商户，请确认 url 是否正确！'
   }
 })
