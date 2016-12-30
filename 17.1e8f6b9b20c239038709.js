bddd5f078c62d5b4f0d4eecc221c2c510fe6fe2c
wangjia <wangjia@raiyee.com>
2016-12-30 11:15:31 +0800

    route meta && run lint fix

--- 17.1e8f6b9b20c239038709.js
+++ 17.1e8f6b9b20c239038709.js
@@ -1,4 +1,4 @@
-webpackJsonp([17],{542:function(module,exports,__webpack_require__){/*!
+webpackJsonp([17],{547:function(module,exports,__webpack_require__){/*!
  * vconsole v2.5.2 (https://github.com/WechatFE/vConsole)
  * Copyright 2016, WechatFE Team
  * MIT license
