b5f1f9794c9f9c7bde40e48ce355978bd28133c2
JounQin <admin@1stg.me>
2017-01-04 17:10:50 +0800

    add title of picker

--- src/views/_Test/Picker/index.js
+++ src/views/_Test/Picker/index.js
@@ -6,6 +6,7 @@ export default require('./index.pug')({
     return {
       pickers: [
         {
+          title: '开始',
           values: ['2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06', '2015-07'],
           className: ['slot1', 'slot4', {slot5: true}],
           textAlign: 'right'
@@ -13,6 +14,7 @@ export default require('./index.pug')({
           divider: '-',
           className: 'slot2'
         }, {
+          title: '结束',
           values: ['2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06'],
           className: 'slot3',
           textAlign: 'left'
